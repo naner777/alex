e5148db31fa5d202256f04ccd6853938aa4b86b5
Renan dos Santos <44875417+naner777@users.noreply.github.com>
2022-06-20 12:26:03 -0300

    Add files via upload

---
+++ exer4.java
@@ -0,0 +1,19 @@
+package exercicio3;
+
+public class exer4 {
+
+    public static void main(String[] orgs){
+        int n1, n2, n3, n4;
+
+        n1 = 3-2-1+2+1+3;
+        n2 = (2 * 3) - (4 * 5);
+        n3 = 2 + 6 - ((3 / 7) * 9);
+        n4 = 3 % 4 - 8;
+
+        System.out.println("O resultado de n1:" + n1);
+        System.out.println("O resultado de n2:" + n2);
+        System.out.println("O resultado de n3:" + n3);
+        System.out.println("O resultado de n4:" + n4);
+    }
+    
+}
